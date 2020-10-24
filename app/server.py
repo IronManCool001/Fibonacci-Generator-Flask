c13e5973c02e814676749965683211bb7c714cc7
unknown <technicalfriend.help@gmail.com>
2020-10-24 14:09:59 +0530

    fixed internal server error

--- app/server.py
+++ app/server.py
@@ -18,10 +18,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/result')
 def result():
     #print(fiboSeries)
-    return render_template('index.html',numbers=fiboSeries)
+    return render_template('result.html',numbers=fiboSeries)
 
 @app.route('/fibonacci', methods=['POST','GET'])
 def showResult():
